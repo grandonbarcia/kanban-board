0146f2797f6ac3e8d3a83691dddd011497cb1fc3
Brandon Garcia <grandonleebarcia@gmail.com>
2023-05-26 17:46:12 -0700

    remove border for columns

--- app/Components/Column.js
+++ app/Components/Column.js
@@ -7,7 +7,7 @@ import { useRef } from 'react';
 
 export default function Column({ column, tasks }) {
   return (
-    <div className="flex flex-col m-8 border-2 rounded w-96">
+    <div className="flex flex-col m-8  rounded w-96">
       <div className="p-8">{column.title}</div>
       <Droppable droppableId={column.id}>
         {(provided, snapshot) => (
