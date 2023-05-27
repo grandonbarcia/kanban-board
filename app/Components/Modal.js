a7ed954e669d324ca125c80c7ce7888397b522c6
Brandon Garcia <grandonleebarcia@gmail.com>
2023-05-26 17:46:50 -0700

    remove mb on modal toggle

--- app/Components/Modal.js
+++ app/Components/Modal.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Modal() {
-  const [showModal, setShowModal] = React.useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
       <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1  ease-linear transition-all duration-150"
         type="button"
         onClick={() => setShowModal(true)}
       >
