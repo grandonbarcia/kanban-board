264c8a7a9a5ece473a24a82bbd047450c45175f6
Brandon Garcia <grandonleebarcia@gmail.com>
2023-06-05 15:28:50 -0700

    added Close button in top right modal

--- app/Components/Modal.js
+++ app/Components/Modal.js
@@ -17,8 +17,31 @@ export default function Modal() {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-1/3 my-6 bg-white p-6 rounded">
               {/*content*/}
-              <div className="text-xl font-bold">
+              <div className="flex leading-9 justify-between text-xl font-bold">
                 <h1>Add New Task</h1>
+                <button
+                  type="button"
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  onClick={() => setShowModal(false)}
+                >
+                  <span class="sr-only">Close menu</span>
+
+                  <svg
+                    class="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
               </div>
 
               <div>
--- app/Components/SideBar.js
+++ app/Components/SideBar.js
@@ -68,7 +68,7 @@ export default function SideBar() {
           <MdOutlineBackupTable className="mr-2" /> Roadmap
         </div>
         {boardNames.map((name) => {
-          return <ListOfBoards title={name} />;
+          return <ListOfBoards title={name} key={name} />;
         })}
         <div className=" pl-10 pt-2 pb- text-purple-500">
           <button
@@ -80,14 +80,35 @@ export default function SideBar() {
           </button>
           {showModal ? (
             <>
-              <div
-                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                onClick={() => setShowModal(false)}
-              >
+              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                 <div className="relative w-1/3 my-6 bg-white p-6 rounded">
                   {/*content*/}
-                  <div className="text-xl font-bold">
+
+                  <div className="flex justify-between leading-9 text-xl font-bold">
                     <h1>Add New Board</h1>
+                    <button
+                      type="button"
+                      class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                      onClick={() => setShowModal(false)}
+                    >
+                      <span class="sr-only">Close menu</span>
+
+                      <svg
+                        class="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
                   </div>
 
                   <div>
