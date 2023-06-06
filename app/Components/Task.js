e8e1f28a84f297dae7a55c5857ee0f5a162a5d65
Brandon Garcia <grandonleebarcia@gmail.com>
2023-06-06 15:37:34 -0700

    added modal to view task and task details

--- app/Components/Task.js
+++ app/Components/Task.js
@@ -1,26 +1,97 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import { useState } from 'react';
 
 const bgWhite = 'p-2 mb-2 border-2 bg-white ';
 const bgGreen = 'p-2 mb-2 border-2 bg-green-400 ';
 
 export default function Task({ task, index, subtasks }) {
-  console.log(Object.keys(subtasks).length);
-  console.log(subtasks);
   const numOfSubtasks = Object.keys(subtasks).length;
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <Draggable draggableId={task.id} index={index}>
-      {(provided, snapshot) => (
-        <div
-          className={snapshot.isDragging ? bgGreen : bgWhite}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-        >
-          <div>{task.content}</div>
-          <div>0/{numOfSubtasks} Subtasks</div>
-        </div>
-      )}
-    </Draggable>
+    <>
+      <Draggable draggableId={task.id} index={index}>
+        {(provided, snapshot) => (
+          <div
+            className={snapshot.isDragging ? bgGreen : bgWhite}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+          >
+            <div className="flex justify-between ">
+              <div>
+                <div>{task.title}</div>
+                <div>0/{numOfSubtasks} Subtasks</div>
+              </div>
+              <div className="my-auto">
+                <button
+                  type="button"
+                  class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"
+                  onClick={() => setShowModal(true)}
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Icon description</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </Draggable>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-1/3 my-6 bg-white p-6 rounded">
+              {/*content*/}
+
+              <div className="flex justify-between leading-9 text-xl font-bold">
+                <h1>{task.title}</h1>
+                <button
+                  type="button"
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  onClick={() => setShowModal(false)}
+                >
+                  <span class="sr-only">Close menu</span>
+
+                  <svg
+                    class="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div>
+                <h2>{task.content}</h2>
+              </div>
+              <div>Subtasks (0/{numOfSubtasks})</div>
+            </div>
+          </div>
+
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
   );
 }
--- app/initial-data.js
+++ app/initial-data.js
@@ -2,7 +2,9 @@ const initialData = {
   tasks: {
     'task-1': {
       id: 'task-1',
-      content: 'Take out the garbage',
+      title: 'Take out the garbage',
+      content:
+        'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
       subtasks: {
         'subtask-1': {
           id: 'subtask-1',
@@ -16,9 +18,27 @@ const initialData = {
         },
       },
     },
-    'task-2': { id: 'task-2', content: 'Watch my favorite show', subtasks: {} },
-    'task-3': { id: 'task-3', content: 'Charge my phone', subtasks: {} },
-    'task-4': { id: 'task-4', content: 'Cook dinner', subtasks: {} },
+    'task-2': {
+      id: 'task-2',
+      title: 'Watch my favorite show',
+      subtasks: {},
+      content:
+        'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
+    },
+    'task-3': {
+      id: 'task-3',
+      title: 'Charge my phone',
+      subtasks: {},
+      content:
+        'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
+    },
+    'task-4': {
+      id: 'task-4',
+      title: 'Cook dinner',
+      subtasks: {},
+      content:
+        'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
+    },
   },
   columns: {
     'column-1': {
