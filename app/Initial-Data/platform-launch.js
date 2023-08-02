2c9780d69158148020dfae7c9981620314107bf2
Brandon Garcia <grandonleebarcia@gmail.com>
2023-08-02 13:31:31 -0700

    removed subtasks from first task

--- app/Initial-Data/platform-launch.js
+++ app/Initial-Data/platform-launch.js
@@ -5,12 +5,9 @@ const initialData = {
       title: 'Take out the garbage',
       content:
         'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
-      subtasks: {
-        'subtask-1': { content: 'Go Outside', complete: false },
-        'subtask-2': { content: 'Throw Garbage', complete: false },
-      },
+      subtasks: {},
       subtasksComplete: 0,
-      subtasksIds: ['subtask-1', 'subtask-2'],
+      subtasksIds: [],
     },
     'task-2': {
       id: 'task-2',
